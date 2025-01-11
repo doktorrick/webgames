bad8e0d87e70cb62e08802a82cd0381f094a69cd
resmith <36131619+samiti3d@users.noreply.github.com>
2025-01-11 16:57:37 +0700

    forge

--- forge.config.js
+++ forge.config.js
@@ -24,6 +24,19 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'doktorrick',
+          name: 'doktorrick'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
